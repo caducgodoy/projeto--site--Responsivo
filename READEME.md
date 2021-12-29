d73aaf5032fe20cd3aaa67c3a4957c0fde27752b
Carlos Eduardo <caducgodoy@hotmail.com>
2021-12-29 09:13:00 -0300

    site feito com responsivo

---
+++ READEME.md
@@ -0,0 +1,2 @@
+Site front-end criado para estudo utilizando Flexbox e tag semanticas para HTML , o java script foi utilizado no menu 
+do site quando vira versao mobile.
---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+window.onload = function(){
+    document.querySelector(".menumobile").addEventListener("click", function(){
+        if(document.querySelector(".menu nav ul").style.display == 'flex') {
+            document.querySelector(".menu nav ul").style.display = 'none';
+        } else {
+            document.querySelector(".menu nav ul").style.display = 'flex';
+        }
+    });
+};
+
+window.addEventListener('resize', function () {
+    var largura = window.innerWidth;
+
+    if (largura > 600){
+       document.querySelector(".menu nav ul").removeAttribute('style');
+    }
+    });
